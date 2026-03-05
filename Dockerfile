64ae34acb6eca20aeb18080fd87040af4d2775ae
manizuntra-eng <mani.zuntra@gmail.com>
2026-03-05 10:32:15 +0530

    fix main.ts

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,20 @@ FROM node:18
 
 WORKDIR /app
 
+# package files copy
 COPY package*.json ./
 
+# install dependencies
 RUN npm install
 
+# copy project
 COPY . .
 
+# build nest project
 RUN npm run build
 
+# expose port
 EXPOSE 5000
 
-CMD ["npm","run","start:prod"]
+# start app
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,15 @@ import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
+  path: path.resolve(__dirname, '../.env'),
 });
-console.log("ENV TEST START");
+
+console.log('ENV TEST START');
 console.log(process.env);
-console.log("ENV TEST END");
+console.log('ENV TEST END');
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.enableCors({
-    origin: '*',
-  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -29,6 +27,11 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
 
-  await app.listen(5000);
+  const port = process.env.PORT || 5000;
+
+  await app.listen(port);
+
+  console.log(`Server running on port ${port}`);
 }
+
 bootstrap();
