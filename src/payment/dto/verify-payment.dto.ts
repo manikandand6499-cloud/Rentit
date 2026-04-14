344aff4e96e7ea94d91f2f83b46f20abf51db1a2
Deva K <poreldeva@gmail.com>
2026-04-14 16:21:35 +0530

    payment small error

--- src/payment/dto/verify-payment.dto.ts
+++ src/payment/dto/verify-payment.dto.ts
@@ -1,20 +1,23 @@
+import { IsString, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
+
 export class VerifyPaymentDto {
   @IsString()
-  razorpay_order_id: string;
+  razorpay_order_id: string | undefined;
 
   @IsString()
-  razorpay_payment_id: string;
+  razorpay_payment_id: string | undefined;
 
   @IsString()
-  razorpay_signature: string;
+  razorpay_signature: string | undefined;
 
   @IsString()
-  planType: string;
+  planType: string | undefined;
 
 @IsNumber()
 @Type(() => Number)
-amount: number;
+amount: number | undefined;
 
   @IsString()
-  propertyType: string;
+  propertyType: string | undefined;
 }
